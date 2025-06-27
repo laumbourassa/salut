27af14d19d58b2084c2df0fb0c49da2758dfd147
Laurent Mailloux-Bourassa <lbourassa95@gmail.com>
2025-06-27 10:07:28 -0400

    Update salut.h

--- src/salut.h
+++ src/salut.h
@@ -1,20 +1,20 @@
 /*
  * salut.h - Statically Allocated Look-Up Table (SALUT) core
- * 
+ *
  * Copyright (c) 2025 Laurent Mailloux-Bourassa
- * 
+ *
  * This file is part of the Statically Allocated Look-Up Table (SALUT) library.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -40,11 +40,15 @@
  *     #undef LUT_SIZE
  *     #undef LUT_FUNC
  */
- 
+
 #ifndef SALUT_H
 #define SALUT_H
 
 #include <stddef.h>
+
+// Extremums supported
+#define _LUT_MIN	(1)
+#define _LUT_MAX	(65535)
 
 // Single-entry macro
 #define _LUT_E(i)	[i] = (LUT_FUNC(i)),
